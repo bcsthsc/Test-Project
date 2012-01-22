6b759d80974579d94d7c6bc27a0718f39f663c8f
Thomas Scholz <thomas@adenburg.de>
2012-01-22 18:39:42 +0100

    Added c

---
+++ src/class_c.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 21, 2012
+
+@author: thos
+'''
+
+class C(object):
+    '''
+    classdocs
+    '''
+
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print "Initialized C object"
--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ Created on Jan 21, 2012
 
 import class_a
 import class_b
+import class_c
+import class_d
 
 if __name__ == '__main__':
     a = class_a.A()
     b = class_b.B()
+    c = class_c.C()
+    d = class_d.D()
